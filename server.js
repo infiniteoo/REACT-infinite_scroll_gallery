9ec70dbd5a198c5fbe2f129cecb94bb3d3af7657
Troy Dorman <troydorman@gmail.com>
2021-09-06 09:33:37 -0600

    created api get route /api/photos to return 30 photos from unsplash api

--- server.js
+++ server.js
@@ -12,4 +12,15 @@ const unsplash = new Unsplash({
 
 const app = express();
 
+app.get("/api/photos", (req, res) => {
+  unsplash.photos
+    .listPhotos(1, 30)
+    .then(toJson)
+    .then((json) => res.json(json));
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
 
