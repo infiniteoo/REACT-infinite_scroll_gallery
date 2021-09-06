dc95e256e486620c80f50287a0c583782d2d294c
Troy Dorman <troydorman@gmail.com>
2021-09-05 20:42:12 -0600

    installed older depenencies. set up varialbes and created an unsplash session

--- config/server.js
+++ config/server.js
@@ -5,4 +5,5 @@ console.log(process.env.APPLICATION_ID, process.env.SECRET);
 module.exports = {
   APPLICATION_ID: process.env.APPLICATION_ID,
   SECRET: process.env.SECRET,
+  CALLBACK_URL: process.env.CALLBACK_URL || "http://localhost:3000",
 };
